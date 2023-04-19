7987c05849962f2a03d3023aecb5bd13429321ff
Yarlenis <yarle88@icloud.com>
2023-04-18 23:03:02 -0400

    first commit

---
+++ salonApp/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Login } from './components/login';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Login/>
+    hola 
+   </div>
+  );
+}
+
+export default App
